acaf21346c809da1cb45fdbf9a48cd4e1299a7f4
Magne Tenstad <tenstad.magne@gmail.com>
2021-06-24 19:38:07 +0200

    Add basic google signin

---
+++ src/lib/GoogleButton.svelte
@@ -0,0 +1,62 @@
+
+<script>
+	import firebase from "firebase/app";
+	import "firebase/auth";
+
+	let loggedIn = false;
+	let name;
+	let uid;
+	
+	// Source: https://firebase.google.com/docs/auth/web/manage-users
+	firebase.auth().onAuthStateChanged((user) => {
+		if (user) {
+			// User is signed in, see docs for a list of available properties
+			// https://firebase.google.com/docs/reference/js/firebase.User
+			uid = user.uid;
+			// ...
+			name = user.displayName;
+			loggedIn = true;
+		} else {
+			// User is signed out
+			// ...
+			loggedIn = false;
+		}
+	});
+
+	// Source: https://firebase.google.com/docs/auth/web/google-signin
+	function signin() {
+		var provider = new firebase.auth.GoogleAuthProvider();
+
+		firebase.auth()
+			.signInWithPopup(provider)
+			.then((result) => {
+				/** @type {firebase.auth.OAuthCredential} */
+				var credential = result.credential;
+
+				// This gives you a Google Access Token. You can use it to access the Google API.
+				var token = credential.accessToken;
+				// The signed-in user info.
+				var user = result.user;
+				// ...
+
+			}).catch((error) => {
+				// Handle Errors here.
+				var errorCode = error.code;
+				var errorMessage = error.message;
+				// The email of the user's account used.
+				var email = error.email;
+				// The firebase.auth.AuthCredential type that was used.
+				var credential = error.credential;
+				// ...
+				console.log(errorCode, errorMessage, email, credential);
+			});
+	}
+</script>
+
+{#if !loggedIn}
+	<button on:click={signin}>
+		Login with Google
+	</button>
+{:else}
+	<p>Signed in as {name}</p>
+{/if}
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,12 +2,33 @@
 	import PollStreamTile from '../lib/PollStreamTile.svelte';
 	import PollStreamTileContainer from '../lib/PollStreamTileContainer.svelte';
 	import FloatingButton from '../lib/FloatingButton.svelte';
+	import GoogleButton from '../lib/GoogleButton.svelte';
+
+	import firebase from "firebase/app";
+
+	const firebaseConfig = {
+		apiKey: "AIzaSyCWhCF-poJ_kAFRk0pfFEKtOdW3aJNMuBQ",
+		authDomain: "poll-stream.firebaseapp.com",
+		projectId: "poll-stream",
+		storageBucket: "poll-stream.appspot.com",
+		messagingSenderId: "70389726858",
+		appId: "1:70389726858:web:ce6df871a0d048ffc13773",
+		measurementId: "G-M76SYZGGMZ"
+	};
+	if (!firebase.apps.length) {
+		firebase.initializeApp(firebaseConfig);
+	} else {
+		firebase.app(); // if already initialized, use that one
+	}
+	
 	let streams = [];
 
 	function appendStreams() {
 		streams = [...streams, streams.length.toString()];
 	}
 </script>
+
+<GoogleButton></GoogleButton>
 
 {#if streams.length == 0}
 	<p style="margin-top: 100px;">No Poll Streams!</p>
