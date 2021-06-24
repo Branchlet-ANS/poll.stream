d3db1ccca56b8ca89dd668a3c1cc6292000b2b12
Eivind <eivindseriksen@gmail.com>
2021-06-24 14:54:51 +0200

    fix merge

--- src/routes/poll_page.svelte
+++ src/routes/poll_page.svelte
@@ -12,20 +12,6 @@
 	<div class="title">New poll</div>
 	<div class="divider"></div>
 	<button class="poll-edit" on:click={handleClick}> counting {answer_count}</button>
-	<nav class="buttonbar">
-		<div class="button">
-			<a href="#/poll">
-				<div class="dot">
-					<img src="/images/poll.png" alt="poll" style="width:2em;height:2em;margin:auto;">
-				</div>
-			</a>
-		</div>
-		<div class="button">
-			<div class="dot">
-				<img src="/images/calendar.png" alt="calendar" style="width:2em;height:2em;margin:auto;">
-			</div>
-		</div>
-	</nav>
 
 </div>
 
