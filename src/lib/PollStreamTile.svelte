ab6eae510404e4e581618e643cd655e115382cb7
Erikseeeeen <43137654+Erikseeeeen@users.noreply.github.com>
2021-06-26 02:50:53 +0200

    add hacked css for tacky appear animations

--- src/lib/PollStreamTile.svelte
+++ src/lib/PollStreamTile.svelte
@@ -1,8 +1,12 @@
 <script>
 	export let remove;
+	let appeardelay = false;
+	setTimeout(function() {
+	  appeardelay = true
+  }, 1);
 </script>
 
-<div class="container">
+<div class="container" class:appeardelay>
 	<h3>Poll Stream Name</h3>
 	<span><slot></slot></span>
 	<p>Poll Stream Details</p>
@@ -12,6 +16,7 @@
 
 <style>
 	.container {
+		position: relative;
 		padding: 10pt;
 		margin-top: 15pt;
         margin-bottom: 0pt;
@@ -24,9 +29,17 @@
 		border-radius: 10pt;
 
 		cursor: pointer;
+
+		filter: blur(10px);
+		transition: filter 0.3s;
+	}
+	.container.appeardelay
+	{
+		filter: blur(0px);
 	}
 
 	.container:hover {
 		box-shadow: 0px 4px 10px var(--c_light);
 	}
+
 </style>
